f1522794bbeeb4be45d9d5868cdd0fc7ea085f1e
ariesxiao <xiaoyaohust@gmail.com>
2025-12-05 08:13:33 -0800

    feat(fetcher): Expose HTMLFetcher in package __init__

--- eng_digest/fetcher/__init__.py
+++ eng_digest/fetcher/__init__.py
@@ -4,5 +4,6 @@ Fetcher module for retrieving articles from various sources.
 
 from .base import Fetcher
 from .rss_fetcher import RSSFetcher
+from .html_fetcher import HTMLFetcher
 
-__all__ = ["Fetcher", "RSSFetcher"]
+__all__ = ["Fetcher", "RSSFetcher", "HTMLFetcher"]
